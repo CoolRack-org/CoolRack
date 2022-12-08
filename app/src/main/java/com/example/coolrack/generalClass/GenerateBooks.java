c39d6fabe0ee89f0acaa53f5224a7843bd897cc1
Jibuza <jibuza@riseup.net>
2022-12-08 10:49:46 +0100

    add metodo createBook, crea los epub analizados en la carpeta personal del programa

--- app/src/main/java/com/example/coolrack/generalClass/GenerateBooks.java
+++ app/src/main/java/com/example/coolrack/generalClass/GenerateBooks.java
@@ -1,5 +1,6 @@
 package com.example.coolrack.generalClass;
 
+import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 
@@ -9,17 +10,19 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.epub.EpubReader;
+import nl.siegmann.epublib.epub.EpubWriter;
 
 public class GenerateBooks {
 
     public GenerateBooks(){}
 
-    public ArrayList<Libro> getLibros() {
+    public ArrayList<Libro> getLibros(Context context) {
         ArrayList<Libro> listBook = null;
 
         try {
@@ -63,6 +66,7 @@ public class GenerateBooks {
                 l.setImg(b.getCoverImage().getInputStream().read());
 
                 listBook.add(l);
+                createBook(b,context);
                 System.out.println(f.getAbsolutePath());
 
             } catch (IOException e) {
@@ -79,5 +83,31 @@ public class GenerateBooks {
         return listBook;
         }
 
+    }
+
+    // Copia los nuevos epubs analizados y los pega en "coleccionLibros"
+    // dentro del direcctorio personal del programa
+    public void createBook(Book libro, Context context){
+        File dirPrivate = context.getFilesDir();
+        File coleccionLibros = new File(dirPrivate,"coleccionLibros");
+
+        if (!coleccionLibros.exists()){
+            coleccionLibros.mkdir();
+        }
+
+        File libroCopy =  new File(coleccionLibros, libro.getTitle()+".epub");
+
+        if (!libroCopy.exists()){
+            try {
+                EpubWriter epubWriter = new EpubWriter();
+                epubWriter.write(libro, new FileOutputStream(libroCopy.getAbsolutePath()));
+                System.out.println(libro.getTitle()+"\n\n");
+            } catch (Exception e) {
+            e.printStackTrace();
+            }
+        }
+        for (File file : coleccionLibros.listFiles()){
+            System.out.println(file.getAbsolutePath());
+        }
     }
 }
